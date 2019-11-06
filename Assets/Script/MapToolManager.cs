db9b87a05d18f9c577a314855a0657e35eab99c9
GUK <gukhyeon10@daum.net>
2019-11-07 01:42:27 +0900

    MapTool (Ver1.0.0)
    그리드 구성 및 타일 배치
    
    스프라이트 아틀라스 생성
    
    해상도 대응 테스트

---
+++ Assets/Script/MapToolManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using System.Xml;
+
+public class MapToolManager : MonoBehaviour
+{
+    [SerializeField]
+    SpriteAtlas tileAtlas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitTile();
+    }
+
+    void InitTile()
+    {
+       
+
+    }
+
+    
+    // 맵 데이터 저장
+    public void MapDataSave()
+    {
+        
+    }
+
+    // 맵 데이터 로드
+    public void MapDataLoad()
+    {
+
+    }
+    
+}
